36a542a66f715e8d49d9f093bc6430a4a6e056fe
hoctor.meng <hoctor.meng@sao.so>
2016-08-16 17:16:41 +0800

    GlobalInterceptor.java and PortalConstants.java

---
+++ src/main/java/com/xinma/portal/common/PortalConstants.java
@@ -0,0 +1,8 @@
+package com.xinma.portal.common;
+
+public class PortalConstants {
+	/**
+	 * 扫码入口，解码标签并获取云端标签信息，并将标签基本信息设置到request attribute
+	 */
+	public final static String tagBasicInfoAttr = "tagBasic";
+}
--- src/main/java/com/xinma/portal/interceptor/GlobalInterceptor.java
+++ src/main/java/com/xinma/portal/interceptor/GlobalInterceptor.java
@@ -3,12 +3,16 @@ package com.xinma.portal.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.xinma.base.tag.CloudTag;
+import com.xinma.base.tag.TagParser;
 import com.xinma.base.util.ServletRequestHelper;
+import com.xinma.portal.common.PortalConstants;
 import com.xinma.portal.config.PortalAppConfig;
 
 /**
@@ -45,7 +49,8 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 		// System.out.println(request.getProtocol());// HTTP/1.1
 		// System.out.println(request.getScheme());// http
 		// System.out.println(request.getServerName());// localhost
-
+		logger.info("ServletPath is {}", request.getServletPath());
+		
 		if ("/favicon.ico".equals(servletPath)) {
 			logger.warn("request servletPath is < /favicon.ico >");
 			return false;
@@ -53,14 +58,8 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 
 		String serverName = request.getServerName();
 		if (serverName.equals(portalAppConfig.getShortDomain())) {
-			// 短域名入口判断是否是微信扫码
-			if (ServletRequestHelper.checkIfOpenInWechat(request)) {
-
-			}
-
-			// 短域名只作为扫码入口域名
-			// CloudTag decodeTag =
-			// TagParser.decodeTagByHiddenCode(cloudTag.getHiddenCode());
+			// 标签扫码校验
+			return tagQueryCheck(request, response);
 
 		} else {
 			String schema = request.getScheme();
@@ -72,7 +71,47 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 				return false;
 			}
 		}
+		
 		return super.preHandle(request, response, handler);
 	}
 
+	private String getHiddenCode(HttpServletRequest request) {
+		String hiddenCode = request.getServletPath();
+		
+		if (StringUtils.isNotBlank(hiddenCode)) {
+			return hiddenCode.substring(1);
+		}
+		
+		return null;
+	}
+	
+	private boolean tagQueryCheck(HttpServletRequest request, HttpServletResponse response) {
+		// 短域名只作为扫码入口域名
+		String hiddenCode = getHiddenCode(request);
+		if(StringUtils.isBlank(hiddenCode)) {
+			// TODO
+			logger.error("servlet path is not correct, path is {}", request.getServletPath());
+			return false;
+		}
+		CloudTag decodeTag = null;
+		 try {
+			decodeTag = TagParser.decodeTagByHiddenCode(hiddenCode);
+			// TODO get tag from ots and check
+		} catch (Exception e) {
+			// TODO
+			logger.error("decode tag by hidden< " + hiddenCode + " > code failed.", e);
+			return false;
+		}
+		
+		// 短域名入口判断是否是微信扫码
+		if (ServletRequestHelper.checkIfOpenInWechat(request)) {
+			// TODO
+			request.setAttribute(PortalConstants.tagBasicInfoAttr, decodeTag);
+		} else {
+			request.setAttribute(PortalConstants.tagBasicInfoAttr, decodeTag);
+		}
+
+
+		return true;
+	}
 }
