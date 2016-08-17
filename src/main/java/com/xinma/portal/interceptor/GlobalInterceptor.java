06671ba11a725fdad1f82ce4047b9b8f9b7485ea
Alauda <hoctor.meng@gmail.com>
2016-08-17 08:39:59 +0800

    新增bean配置文件

--- src/main/java/com/xinma/portal/interceptor/GlobalInterceptor.java
+++ src/main/java/com/xinma/portal/interceptor/GlobalInterceptor.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.xinma.base.datastore.ext.table.TagTableService;
 import com.xinma.base.tag.CloudTag;
 import com.xinma.base.tag.TagParser;
 import com.xinma.base.util.ServletRequestHelper;
@@ -28,10 +29,20 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 	@Autowired
 	private PortalAppConfig portalAppConfig;
 
+	@Autowired
+	private TagTableService tagTableService;
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// TODO Auto-generated method stub
+		// System.out.println(request.getRequestURL()); //
+		// http://localhost:8080/portal/hello/world
+		// System.out.println(request.getRequestURI()); // /portal/hello/world
+		// System.out.println(request.getPathInfo()); // /hello/world
+		// System.out.println(request.getProtocol());// HTTP/1.1
+		// System.out.println(request.getScheme());// http
+		// System.out.println(request.getServerName());// localhost
 
 		// 获取用户IP
 		// 获取servlet path，便于统计访问量
@@ -42,15 +53,8 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 		logger.info("access into blackListInterceptor, client ip is <{}>,, servlet path is <{}>", clientIp,
 				servletPath);
 
-		// System.out.println(request.getRequestURL()); //
-		// http://localhost:8080/portal/hello/world
-		// System.out.println(request.getRequestURI()); // /portal/hello/world
-		// System.out.println(request.getPathInfo()); // /hello/world
-		// System.out.println(request.getProtocol());// HTTP/1.1
-		// System.out.println(request.getScheme());// http
-		// System.out.println(request.getServerName());// localhost
 		logger.info("ServletPath is {}", request.getServletPath());
-		
+
 		if ("/favicon.ico".equals(servletPath)) {
 			logger.warn("request servletPath is < /favicon.ico >");
 			return false;
@@ -71,30 +75,30 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 				return false;
 			}
 		}
-		
+
 		return super.preHandle(request, response, handler);
 	}
 
 	private String getHiddenCode(HttpServletRequest request) {
 		String hiddenCode = request.getServletPath();
-		
+
 		if (StringUtils.isNotBlank(hiddenCode)) {
 			return hiddenCode.substring(1);
 		}
-		
+
 		return null;
 	}
-	
+
 	private boolean tagQueryCheck(HttpServletRequest request, HttpServletResponse response) {
 		// 短域名只作为扫码入口域名
 		String hiddenCode = getHiddenCode(request);
-		if(StringUtils.isBlank(hiddenCode)) {
+		if (StringUtils.isBlank(hiddenCode)) {
 			// TODO
 			logger.error("servlet path is not correct, path is {}", request.getServletPath());
 			return false;
 		}
 		CloudTag decodeTag = null;
-		 try {
+		try {
 			decodeTag = TagParser.decodeTagByHiddenCode(hiddenCode);
 			// TODO get tag from ots and check
 		} catch (Exception e) {
@@ -102,7 +106,7 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 			logger.error("decode tag by hidden< " + hiddenCode + " > code failed.", e);
 			return false;
 		}
-		
+
 		// 短域名入口判断是否是微信扫码
 		if (ServletRequestHelper.checkIfOpenInWechat(request)) {
 			// TODO
@@ -110,7 +114,6 @@ public class GlobalInterceptor extends HandlerInterceptorAdapter {
 		} else {
 			request.setAttribute(PortalConstants.tagBasicInfoAttr, decodeTag);
 		}
-
 
 		return true;
 	}
